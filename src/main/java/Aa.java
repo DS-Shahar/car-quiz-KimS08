9200922e24bac695a46461e354db53c7eeb0e13e
KimS08 <kimstrugo2@gmail.com>
2024-10-06 12:51:39 +0300

    Update Aa.java

--- src/main/java/Aa.java
+++ src/main/java/Aa.java
@@ -2,14 +2,14 @@
 	public boolean illegal(int maxSpeed) {
 		if (this.getSpeed() > maxSpeed)
 			return true;
-		else if (this.getPrivateCar == true)
+		else if (this.getPrivateCar() == true)
 			return true;
 		return false;
 	}
 	
 	public boolean allGood() {
 		for (int i=0; i < this.cars.length; i++) {
-			if (this.cars[i].illegal(this.getMaxSpeed) == true)
+			if (this.cars[i].illegal(this.getMaxSpeed()) == true)
 				return false;
 		}
 		return true;
